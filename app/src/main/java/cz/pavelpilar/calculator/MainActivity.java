cd65600f06f3278140f1987180ae6a7dbf783f2a
Pavel Pilar <pavel.pilar1@gmail.com>
2015-12-28 19:15:40 +0100

    Added settings

--- app/src/main/java/cz/pavelpilar/calculator/MainActivity.java
+++ app/src/main/java/cz/pavelpilar/calculator/MainActivity.java
@@ -1,32 +1,66 @@
 package cz.pavelpilar.calculator;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Handler;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import cz.pavelpilar.calculator.calculator.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private static Context mContext;
+    private DrawerLayout mDrawerLayout;
+    private LinearLayout mDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mContext = this;
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));  // Action bar is needed to get menu overflow button
+        getSupportActionBar().setTitle("");
 
-        Fragment fragment = new MainFragment();
+        mContext = this;
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawer = (LinearLayout) findViewById(R.id.drawer);
+        Calculator.initialize();
+
+        Fragment fragment = new cz.pavelpilar.calculator.calculator.MainFragment();
         FragmentManager manager = getSupportFragmentManager();
         manager.beginTransaction()
                .replace(R.id.content_frame, fragment)
                .commit();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.calculator_menu, menu);
+        return true;
+    }
+
     public static Context getContext(){
         return mContext;
+    }
+
+    public void settings(View v) {
+        new Handler().postDelayed(new Runnable() {  //Avoids lag
+            @Override
+            public void run() {
+                startActivity(new Intent(getContext(), SettingsActivity.class));
+            }
+        }, 150);
+        mDrawerLayout.closeDrawer(mDrawer);
     }
 }
---
+++ app/src/main/java/cz/pavelpilar/calculator/SettingsActivity.java
@@ -0,0 +1,50 @@
+package cz.pavelpilar.calculator;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.settings_toolbar));
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.settings_toolbarDark));
+        getFragmentManager().beginTransaction()
+                            .replace(R.id.settings_content, new SettingsFragment())
+                            .commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+
+            findPreference("pref_clear_history").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    //TODO: clear history
+                    return false;
+                }
+            });
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        if (menuItem.getItemId() == android.R.id.home) finish();
+        return super.onOptionsItemSelected(menuItem);
+    }
+}
